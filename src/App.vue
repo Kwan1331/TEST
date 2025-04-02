8a9c9242dd2694e539161971d8c34d172f9ae28c
Kaiyuan Wang <kwan133@student.monash.edu>
2025-04-02 22:59:30 +1100

    add two images

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You database" />
+      <HelloWorld msg="the name has been changed in kaiyuan branch" />
     </div>
   </header>
 
